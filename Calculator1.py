aeebd3f438309eaa7577ee254c70dc7b1ca5cf6e
Satvika09 <102314187+Satvika09@users.noreply.github.com>
2022-04-05 22:26:28 +0530

    Create Calculator1.py

---
+++ Calculator1.py
@@ -0,0 +1,9 @@
+n1=int(input("enter first operand:"))
+n2=int(input("enter second operand:"))
+op=input("enter a operator:")
+if op=='+':
+    print("addition of two operands:",n1+n2)
+elif op=='-':
+    print("subtraction of two operands:",n1-n2)
+else:
+    print("invalid operator")
