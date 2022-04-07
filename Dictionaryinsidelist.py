f4751a80cdc5f66247e7cf77f39ad48f65110c91
Satvika09 <102314187+Satvika09@users.noreply.github.com>
2022-04-07 19:30:29 +0530

    Create Dictionaryinsidelist.py

---
+++ Dictionaryinsidelist.py
@@ -0,0 +1,8 @@
+dictionary1 = {'reptiles':'sanke','mammals':'whale','amphibians':'frogs'}
+
+list1 = ['tiger', 'lion']
+
+dictionary1_copy = dictionary1.copy()
+list1.append(dictionary1_copy)
+
+print(list1)
