db9b7b8df0db4b28db84a38adbd89037881b0e00
Satvika09 <102314187+Satvika09@users.noreply.github.com>
2022-04-12 19:14:03 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -42,4 +42,73 @@ car = {
   "year": 1964
 }
 x = car.copy()
-print(x)
+print('coping the car:',x)
+  
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del car["model"]
+print('after deleting values left:',car)
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+print('after poping model left values in car:',car)
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.popitem()
+print('after popitem left car values:',car)
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.keys()
+print('oringinal car:',x) #before the change
+car["color"] = "white"
+print('after modifying car:',x) #after the change
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car["brand"])
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print('items in car:')
+for x, y in car.items():
+ print(x, y)
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = car.get("model")
+print('getting the model value:',x)
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = car.setdefault("color", "white")
+print('default value:',x)
+print('new car:',car)
+
+
